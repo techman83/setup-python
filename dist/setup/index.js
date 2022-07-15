61e049aff61c765fe2e18905042d29f2cc86927b
Leon Wright <techman83@gmail.com>
2022-07-15 12:57:51 +0800

    fix: Avoid clobbering RUNNER_TOOL_PATH
    A change was made to fix a long standing set of issues, relating to the
    tool path that actions/python-versions historically used. However this
    was fixed in
    actions/python-versions@5815b5c91958f7d75370a6bdcb549133c64aa13c and is
    no longer required.
    
        Fixes #459

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -65276,17 +65276,7 @@ function resolveVersionInput() {
     return version;
 }
 function run() {
-    var _a;
     return __awaiter(this, void 0, void 0, function* () {
-        // According to the README windows binaries do not require to be installed
-        // in the specific location, but Mac and Linux do
-        if (!utils_1.IS_WINDOWS && !((_a = process.env.AGENT_TOOLSDIRECTORY) === null || _a === void 0 ? void 0 : _a.trim())) {
-            if (utils_1.IS_LINUX)
-                process.env['AGENT_TOOLSDIRECTORY'] = '/opt/hostedtoolcache';
-            else
-                process.env['AGENT_TOOLSDIRECTORY'] = '/Users/runner/hostedtoolcache';
-            process.env['RUNNER_TOOL_CACHE'] = process.env['AGENT_TOOLSDIRECTORY'];
-        }
         core.debug(`Python is expected to be installed into RUNNER_TOOL_CACHE=${process.env['RUNNER_TOOL_CACHE']}`);
         try {
             const version = resolveVersionInput();
